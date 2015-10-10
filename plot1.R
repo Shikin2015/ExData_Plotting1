a21895573378029728b335155c0850d4a624add0
ashikin.ujang <ashikin.ujang@gmail.com>
2015-10-10 18:57:25 +0800

    Submitting Exploratory Data Analysis Course Project 1

---
+++ plot1.R
@@ -0,0 +1,8 @@
+setwd("F:/Module4/CourseProject1")
+datahouse <- "F:/Module4/CourseProject1/household_power_consumption.txt"
+data <- read.table(datahouse, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subsetdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+globalActivePower <- as.numeric(subsetdata$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
